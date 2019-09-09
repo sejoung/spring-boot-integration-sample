bd5a99a699a68ada3554491aefcdc8f172cb3c77
sejoung <sejoung@gmail.com>
2019-09-09 16:23:41 +0900

    추가

--- src/main/java/com/github/sejoung/Application.java
+++ src/main/java/com/github/sejoung/Application.java
@@ -1,12 +1,9 @@
 package com.github.sejoung;
 
-import com.github.sejoung.integration.constants.ServiceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ServiceProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/sejoung/integration/constants/ServiceProperties.java
+++ src/main/java/com/github/sejoung/integration/constants/ServiceProperties.java
@@ -1,9 +1,11 @@
 package com.github.sejoung.integration.constants;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedResource;
 
+@EnableConfigurationProperties(ServiceProperties.class)
 @ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
 @ManagedResource
 public class ServiceProperties {
